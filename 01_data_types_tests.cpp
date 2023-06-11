eb827a9ef8a3ef7d740c1d9537ac27efaebb483e
Mariocoro <135136166+Mariocoro@users.noreply.github.com>
2023-06-10 22:41:49 -0500

    Add files via upload

---
+++ 01_data_types_tests.cpp
@@ -0,0 +1,17 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+#include "data_types.h"
+TEST_CASE("Verify Test Configuration", "verification") {
+	REQUIRE(true == true);
+}
+TEST_CASE("Verify sum_numbers function") 
+{
+	REQUIRE(add_numbers(5, 5) == 10);
+	REQUIRE(add_numbers(10, 10) == 20);
+}
+
+TEST_CASE("Test Multiply Numbers")
+{
+	REQUIRE(multiply_numbers(10) == 50);
+	REQUIRE(multiply_numbers(2) == 10);
+}
---
+++ main.cpp
@@ -0,0 +1,21 @@
+﻿//write include statements
+#include <iostream>
+#include "data_types.h"
+
+//write namespace using statement for cout
+using std::cout;
+using std::cin;
+
+int main()
+{
+	int num;
+	cout<<" enter a number for num:";
+	cin>>num;
+	int result = multiply_numbers(num);
+	cout<<"the result is "<<result<<"n";
+	int num1=4;
+	result = multiply_numbers(num1);
+	cout<<"the result is:"<<result<<"\n";
+
+	return 0;
+}
