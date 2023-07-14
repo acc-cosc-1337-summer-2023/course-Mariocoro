979392f742bd403007b8ed0a305ea76d5ca7e782
Mariocoro <135136166+Mariocoro@users.noreply.github.com>
2023-07-14 02:03:04 +0000

    homework3

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,41 @@
 //write include statement for decisions header
+#include "decisions.h"
 
+using std::string;
+
+//Write code for function(s) code here
+
+string get_letter_grade_using_switch(int grade)
+{
+    string option = "";
+
+    switch (grade)
+    {
+    case 90 ... 100: 
+        option = "A";
+        break;
+
+    case 80 ... 89: 
+        option = "B";
+        break;
+
+    case 70 ... 79: 
+        option = "C";
+        break;
+
+    case 60 ... 69: 
+        option = "D";
+        break;
+
+    case 00 ... 59: 
+        option = "F";
+        break;
+
+    default:
+        option = "Invalid Option";
+        break;
+    }
+    return option;
+}
 
 //Write code for function(s) code here
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,18 @@
-//write include statements
+#include<iostream>
+#include "decisions.h"
+
+using std::cout;
+using std::cin;
+using std::string;
 
 int main() 
 {
+	int grade;
+	cout<<"Please enter a number for grade: ";
+	cin>>grade;
+	
+	string option = get_letter_grade_using_switch(grade);
+	cout<<"The letter grade is: "<<option<<"\n";
+	
 	return 0;
 }
